3c93ad9fb1ea64194349e02d071cfe81f2c7c973
Bryan Lai <bryanlais@gmail.com>
2023-10-29 21:53:10 +0800

    Add some trivial refactor

--- pr-commit.user.js
+++ pr-commit.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @include     /github\.com\/([\w-]+\/[\w-]+)\/pull\/(\d+)/
 // @grant       none
-// @version     1.0
+// @version     1.1
 // @author      Bryan Go
 // @description 10/29/2023, 11:47:39 AM
 // ==/UserScript==
@@ -15,9 +15,9 @@
     /^.*github\.com\/([\w-]+\/[\w-]+)\/pull\/(\d+).*$/
   )
 
-  const apiLink = `https://api.github.com/repos/${prRepo}/pulls/${prNumber}`
+  const prApi = `https://api.github.com/repos/${prRepo}/pulls/${prNumber}`
   const isNixpkgs = document.URL.includes(
-    'github.com/NixOS/nixpkgs/pull'
+    'github.com/NixOS/nixpkgs/pull/'
   )
 
   const processResponse = json => {
@@ -34,46 +34,48 @@
     prInfoLine.innerHTML +=
       `&ensp;<a href="${commitLink}"><code class="Link--primary text-bold">${commitShort}</code></a>`
 
-    if (isNixpkgs) {
-      const compareLink = `https://github.com/NixOS/nixpkgs/compare/${commitHash}`
-      const compareApi = `https://api.github.com/repos/NixOS/nixpkgs/compare/${commitHash}`
-      const withMaster = `${compareApi}...master`
-      const withNixpkgs = `${compareApi}...nixpkgs-unstable`
-      const withNixos = `${compareApi}...nixos-unstable`
-
-      const masterId = 'compare-master'
-      const nixpkgsId = 'compare-nixpkgs'
-      const nixosId = 'compare-nixos'
-      prInfoLine.innerHTML +=
-        `&ensp;<a href="${compareLink}...master" id="${masterId}"><b>master</b></a>`
-      prInfoLine.innerHTML +=
-        `&ensp;<a href="${compareLink}...nixpkgs-unstable" id="${nixpkgsId}"><b>nixpkgs-unstable</b></a>`
-      prInfoLine.innerHTML +=
-        `&ensp;<a href="${compareLink}...nixos-unstable" id="${nixosId}"><b>nixos-unstable</b></a>`
-
-      const branchIndicate = (success, idSelector) => {
-        const indicator = document.querySelector(`#${idSelector}`)
-        if (success) {
-          indicator.outerHTML = '✅ ' + indicator.outerHTML
-        } else {
-          indicator.outerHTML = '⚠️ ' + indicator.outerHTML
-        }
-      }
-
-      const fetchBranchStatus = (link, idSelector) => fetch(`${link}?per_page=1`)
-        .then(async (response) => await response.text())
-        .then((text) => JSON.parse(text))
-        .then((json) => json.status === 'ahead')
-        .then((success) => { branchIndicate(success, idSelector) })
-        .catch((e) => { console.log(e) })
-
-      fetchBranchStatus(withMaster, masterId)
-      fetchBranchStatus(withNixpkgs, nixpkgsId)
-      fetchBranchStatus(withNixos, nixosId)
+    if (!isNixpkgs) {
+      return
     }
+    // probabaly need to be refactored
+    const compareLink = `https://github.com/NixOS/nixpkgs/compare/${commitHash}`
+    const compareApi = `https://api.github.com/repos/NixOS/nixpkgs/compare/${commitHash}`
+    const withMaster = `${compareApi}...master`
+    const withNixpkgs = `${compareApi}...nixpkgs-unstable`
+    const withNixos = `${compareApi}...nixos-unstable`
+
+    const masterId = 'compare-master'
+    const nixpkgsId = 'compare-nixpkgs'
+    const nixosId = 'compare-nixos'
+    prInfoLine.innerHTML +=
+      `&ensp;<a href="${compareLink}...master" id="${masterId}"><b>master</b></a>`
+    prInfoLine.innerHTML +=
+      `&ensp;<a href="${compareLink}...nixpkgs-unstable" id="${nixpkgsId}"><b>nixpkgs-unstable</b></a>`
+    prInfoLine.innerHTML +=
+      `&ensp;<a href="${compareLink}...nixos-unstable" id="${nixosId}"><b>nixos-unstable</b></a>`
+
+    const branchIndicate = (success, idSelector) => {
+      const indicator = document.querySelector(`#${idSelector}`)
+      if (success) {
+        indicator.outerHTML = '✅ ' + indicator.outerHTML
+      } else {
+        indicator.outerHTML = '⚠️ ' + indicator.outerHTML
+      }
+    }
+
+    const fetchBranchStatus = (link, idSelector) => fetch(`${link}?per_page=1`)
+      .then(async (response) => await response.text())
+      .then((text) => JSON.parse(text))
+      .then((json) => json.status === 'ahead')
+      .then((success) => { branchIndicate(success, idSelector) })
+      .catch((e) => { console.log(e) })
+
+    fetchBranchStatus(withMaster, masterId)
+    fetchBranchStatus(withNixpkgs, nixpkgsId)
+    fetchBranchStatus(withNixos, nixosId)
   }
 
-  fetch(apiLink)
+  fetch(prApi)
     .then(async (response) => await response.text())
     .then((text) => JSON.parse(text))
     .then((json) => { processResponse(json) })
