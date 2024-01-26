361abc4fc7327fff3452641b95b483c962191d28
Bryan Lai <bryanlais@gmail.com>
2024-01-26 14:30:00 +0800

    Revamp script for publication

--- src/arxiv-inspirehep.user.js
+++ src/arxiv-inspirehep.user.js
@@ -1,95 +1,68 @@
 // ==UserScript==
 // @name         arXiv with inspirehep
-// @namespace    http://tampermonkey.net/
-// @version      0.4
-// @author       Bryan
+// @namespace    Violentmonkey Scripts
 // @match        https://arxiv.org/abs/*
+// @version      0.5
+// @author       Bryan Lai <bryanlais@gmail.com>
 // @grant        GM.setClipboard
 // @grant        GM.xmlHttpRequest
 // ==/UserScript==
 
-(function() {
-  'use strict';
+/* global GM */
+(function () {
+  'use strict'
 
-  // capture keyup
-  document.addEventListener("keyup", function(event) {
-      // <Enter> key
-      if (event.keyCode === 13) {
-          // cancel the default action, if needed
-          event.preventDefault();
-          // trigger new action: open PDF
-          let pdf_button = document.getElementsByClassName("download-pdf")[0];
-          pdf_button.target = "_blank";
-          pdf_button.click();
-      }
-  });
+  // capture <Enter> to open PDF
+  document.addEventListener('keyup', event => {
+    if (event.key === 'Enter') {
+      // cancel the default action, if needed
+      event.preventDefault()
+
+      // trigger new action
+      const pdfButton = document.getElementsByClassName('download-pdf')[0]
+      pdfButton.target = '_blank'
+      pdfButton.click()
+    }
+  })
 
   // copy authors
-  let authors = document.querySelector("div.authors");
+  const authors = document.querySelector('div.authors')
   if (authors) {
-      GM.setClipboard(
-          "[" + authors.innerText + "]"
-      );
-      // abbreviated
-      authors = Array.from(authors.querySelectorAll("a"));
-      GM.setClipboard(
-          "["
-          + authors.map(
-              author => author.innerText.split(' ').at(-1)
-          ).join(", ")
-          + "]"
-      );
+    // first, send full names to clipboard
+    GM.setClipboard(`[${authors.innerText}]`)
+
+    // then, send last names to clipboard as well
+    const surnames =
+      Array
+        .from(authors.querySelectorAll('a'))
+        .map(
+          author => author.innerText.split(' ').at(-1)
+        )
+    GM.setClipboard(`[${surnames.join(', ')}]`)
   };
 
+  // inspirehep citations overview
+  const refCiteElement = document.getElementsByClassName('extra-ref-cite')[0]
+  const inspirehepUrl =
+    Array
+      .from(refCiteElement.getElementsByTagName('a'))
+      .filter(x => x.href.includes('inspirehep'))[0].href
+      .replace(
+        'https://inspirehep.net/arxiv',
+        'https://inspirehep.net/api/arxiv'
+      )
 
-  // function waitExec(trigger_func, func, marker) {
-  //     if (trigger_func() == "EXIT") {
-  //         return;
-  //     }
-  //     if (typeof trigger_func() !== "undefined" && trigger_func() !== null && trigger_func() != '') {
-  //         func();
-  //     } else {
-  //         console.log('Waiting for:', marker, '/ now:', trigger_func());
-  //         setTimeout(function() {
-  //             waitExec(trigger_func, func, marker);
-  //         }, 200);
-  //     }
-  // }
-
-  function getTarget() {
-      return document.getElementsByClassName("extra-ref-cite")[0];
-  }
-
-  // waitExec(
-  //     getTarget,
-  //     function () {
-
-          let links = Array.from(document.getElementsByClassName("extra-ref-cite")[0].getElementsByTagName("a"));
-          let inspirehep_url = links.filter(x => x.href.includes("inspirehep"))[0].href;
-
-          // New API
-          inspirehep_url = inspirehep_url.replace(
-              "https://inspirehep.net/arxiv",
-              "https://inspirehep.net/api/arxiv"
-          );
-
-          let req = GM.xmlHttpRequest({
-            method: "GET",
-            url: inspirehep_url,
-            onload: function(response) {
-                // console.log(inspirehep_url);
-                // console.log(response);
-                var cites = JSON.parse(response.responseText).metadata.citation_count;
-                // console.log("### inspirehep citations:", cites);
-                getTarget().children[0].innerHTML
-                    += '<span style="color: #b31b1b; font-weight: bold;"> {'
-                    + cites
-                    + "} </span>";
-            }
-          });
-
-  //     },
-  //     "citations div"
-  // );
-
-})();
+  // cross origin request to inspirehep
+  GM.xmlHttpRequest({
+    method: 'GET',
+    url: inspirehepUrl,
+    onload: function (response) {
+      // console.log(inspirehepUrl);
+      // console.log(response);
+      const cites = JSON.parse(response.responseText).metadata.citation_count
+      // console.log("### inspirehep citations:", cites);
+      refCiteElement.children[0].innerHTML +=
+        `<span style="color: #b31b1b; font-weight: bold;"> ${cites} </span>`
+    }
+  })
+})()
